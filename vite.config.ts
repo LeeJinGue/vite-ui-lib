03b2029973e30e68f5f6d78c2f83fcbcb8e0114b
gyu9851 <gyu9851@bigtech.co.kr>
2024-04-15 13:53:39 +0900

    build: remove css inject

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve, relative, extname } from "path";
 import dts from "vite-plugin-dts";
-import { libInjectCss } from "vite-plugin-lib-inject-css";
+// import { libInjectCss } from "vite-plugin-lib-inject-css";
 import { fileURLToPath } from "node:url";
 import { glob } from "glob";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), vanillaExtractPlugin(), libInjectCss(), dts({ include: ["lib"] })],
+    plugins: [
+        react(),
+        vanillaExtractPlugin(),
+        // libInjectCss(),
+        dts({ include: ["lib"] }),
+    ],
     build: {
         lib: {
             entry: resolve(__dirname, "lib/main.ts"),
